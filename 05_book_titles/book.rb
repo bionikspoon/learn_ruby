6da6b2cfc25139e6d12ad2e441acc622058794ee
Manu Phatak (bionikspoon) <bionikspoon@gmail.com>
2016-06-29 18:54:26 -0500

    book titles

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,16 @@
+def titleize text
+  capitalize = lambda do |word, i|
+    skip = ('and the over in of a an'.split.include?(word) and not i == 0)
+    skip ? word : word.capitalize
+  end
+
+  text.split(' ').map.with_index(&capitalize).join(' ')
+end
+
 class Book
-# write your code here
+  attr_accessor :title
+
+  def title=(value)
+    @title = titleize(value)
+  end
 end
