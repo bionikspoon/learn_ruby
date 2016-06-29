17c746a03133f4d0d484c4945358b20606bb19d7
Manu Phatak (bionikspoon) <bionikspoon@gmail.com>
2016-06-29 16:24:50 -0500

    passing hello, temp, and calc

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,23 @@
-#write your code here
+def add x, y
+  x+y
+end
+
+def subtract x, y
+  x-y
+end
+
+def sum items
+  items.reduce(:+) or 0
+end
+
+def multiply *args
+  args.reduce(:*)
+end
+
+def power x, y
+  x ** y
+end
+
+def factorial x
+  x == 0 ? 1 : ((1..x).each.reduce :*)
+end
