a6de2def9bddcc1891b25d1e4f2b94daab549584
Manu Phatak (bionikspoon) <bionikspoon@gmail.com>
2016-06-29 17:19:23 -0500

    simon says

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,28 @@
-#write your code here
+def echo text
+  text
+end
+
+def shout text
+  text.upcase
+end
+
+def repeat text, count=2
+  ([text] * count).join(' ')
+end
+
+def start_of_word text, length
+  text[0, length]
+end
+
+def first_word text
+  text.split(' ').first
+end
+
+def titleize text
+  capitalize = lambda do |word, i|
+    skip = ('and the over'.split.include?(word) and not i == 0)
+    skip ? word : word.capitalize
+  end
+
+  text.split(' ').map.with_index { |word, i| capitalize.call(word, i) }.join(' ')
+end
