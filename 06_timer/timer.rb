4fa143394ca4ed2afacae67949f88464576dfc05
Manu Phatak (bionikspoon) <bionikspoon@gmail.com>
2016-06-29 19:04:35 -0500

    timer

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,13 @@
 class Timer
-  #write your code here
+  attr_accessor :seconds
+
+  def initialize
+    @seconds = 0
+  end
+
+  def time_string
+    hours, seconds = @seconds.divmod(60*60)
+    minutes, seconds = seconds.divmod(60)
+    "%02d:%02d:%02d" % [hours, minutes, seconds]
+  end
 end
